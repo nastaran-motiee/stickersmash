5c6af09154da9d658902e7edcd199c1e35414f44
Nastaran Motiee <nnastaran83@gmail.com>
2025-05-28 13:09:48 +0300

    refactor: update AboutScreen component to enhance content and styling

--- app/(tabs)/about.tsx
+++ app/(tabs)/about.tsx
@@ -1,21 +1,29 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function AboutScreen() {
-    return (
-        <View style={ styles.container }>
-            <Text style={ styles.text }>About screen</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Add stickers to your image using</Text>
+      <Text style={styles.textColorfull}>STICKER SMASH</Text>
+      <Text style={styles.textColorfull}>📸</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    text: {
-        color: '#fff',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#fff",
+    margin: 10,
+    fontSize: 15,
+  },
+  textColorfull: {
+    color: "#ffd33d",
+    fontSize: 24,
+  },
 });
